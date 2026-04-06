f9c22d9f3dbc8c332bf9c38cd3a0e1fa9daedd60
Arayik Gharibyan <arayik.gharibyan.1999@gmail.com>
2026-04-06 23:03:32 +0400

    Add Zephyr Nucleo-F429ZI project setup Track Zephyr sources through west manifest Added west config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
 int main()
 {
-    std::cout << "Hello from STM32!" << std::endl;
+    printk("Hello from STM32 Nucleo-F429ZI on Zephyr!\n");
+
+    while (true) {
+        printk("Application main() is running\n");
+        k_sleep(K_SECONDS(1));
+    }
+
     return 0;
 }
